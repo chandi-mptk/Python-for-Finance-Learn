c55aa0df398217d980d2ad5bbe6a355c2ab52dc2
chandi-mptk <65593076+chandi-mptk@users.noreply.github.com>
2022-01-22 22:23:02 +0530

    Learning Python for Finance

--- Python-Finance-L2.py
+++ Python-Finance-L2.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import requests
+import matplotlib.pyplot as plt
 
 # Series
+
+
 print('==============================================================================================================')
 print('# Series\n')
 prices = [42.8, 102.03, 240.38, 80.9]
@@ -15,8 +19,8 @@ print('=========================================================================
 print("# DataFrame\n")
 
 data = {
-  'date': ['2021-06-10', '2021-06-11', '2021-06-12', '2021-06-13'],
-  'prices': [42.8, 102.03, 240.38, 80.9]
+    'date': ['2021-06-10', '2021-06-11', '2021-06-12', '2021-06-13'],
+    'prices': [42.8, 102.03, 240.38, 80.9]
 }
 df = pd.DataFrame(data)
 print(df)
@@ -32,14 +36,46 @@ print("\nAll Tables as DataFrame\n")
 print(df)
 
 # Web Scraping
-print('\n==============================================================================================================')
+print(
+    '\n==============================================================================================================')
 print("# Web Scraping 2")
 df1 = df[['Symbol', 'Security']]
 print(df1)
-print('\n==============================================================================================================')
+print(
+    '\n==============================================================================================================')
 df2 = df1[df['Security'] == 'Apple']
 print(df2)
-print('\n==============================================================================================================')
+print(
+    '\n==============================================================================================================')
 print(df.info())
+print(
+    '\n==============================================================================================================')
+
+# Yahoo Finance Tesla Profile
+print(
+    '\n==============================================================================================================')
+print("# Yahoo Finance Tesla Profile\n")
+url_link = 'https://finance.yahoo.com/quote/TSLA/profile'
+r = requests.get(url_link, headers={
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'})
+data = pd.read_html(r.text)
+print(data)
+
+# Yahoo Finance Tesla Analysis
+print('\n==============================================================================================================')
+print("# Yahoo Finance Tesla Analysis\n")
+url_link = 'https://finance.yahoo.com/quote/TSLA/analysis?p=TSLA'
+r = requests.get(url_link, headers={
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'})
+data = pd.read_html(r.text)
+data = data[0]
+print(data)
+data1 = data[data['Earnings Estimate'] == 'Avg. Estimate']
+print(data1)
+data1.plot(kind='bar')
+
+plt.savefig('Tesla_Analysis.png')
+
+# Yahoo Finance Tesla Analysis
 print('\n==============================================================================================================')
 
